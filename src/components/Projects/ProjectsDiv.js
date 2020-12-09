349a03d956d95ed6b1f665d94e2e5489e3d58347
rbcabreraUPM <rbcabrera1@up.edu.ph>
2020-12-09 21:56:09 +0800

    added images

--- src/components/Projects/ProjectsDiv.js
+++ src/components/Projects/ProjectsDiv.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { Col, Row, Grid } from 'react-flexbox-grid'
 import DeviceIcons from './DeviceIcons'
 import ProjectCards from './ProjectCards'
+
+import profilePhoto from '../../static/img/profile.jpg'
 const ProjectsDiv = ({ projects }) => {
   const group = (items, n) =>
     items.reduce((acc, x, i) => {
@@ -12,12 +14,22 @@ const ProjectsDiv = ({ projects }) => {
 
   return (
     <div>
-      {group(projects, 3).map((children) => (
+      {group(projects, 2).map((children) => (
         <ul className='cards__items_custom'>
           {children.map((x, i) => (
             <div className='card_custom card-project_custom'>
               <Row>
-                <Col>
+                <Col xs={3} sm={3} md={3} lg={3}>
+                  <div>
+                    {' '}
+                    <img
+                      src={`/images/projects/${x.image}`}
+                      alt='/'
+                      style={{ height: 'auto', maxWidth: '100%' }}
+                    ></img>
+                  </div>
+                </Col>
+                <Col xs={9} sm={9} md={9} lg={9}>
                   <div>
                     <h2>
                       {x.name} <DeviceIcons devices={x.devices}></DeviceIcons>
